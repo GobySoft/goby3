aaa779b2ba10683a7767b4b121558c7017725534
Toby Schneider <toby@gobysoft.org>
2018-03-12 17:11:34 -0400

    Added geodesy library

---
+++ src/test/util/geodesy/test.cpp
@@ -0,0 +1,77 @@
+// Copyright 2009-2018 Toby Schneider (http://gobysoft.org/index.wt/people/toby)
+//                     GobySoft, LLC (2013-)
+//                     Massachusetts Institute of Technology (2007-2014)
+//
+//
+// This file is part of the Goby Underwater Autonomy Project Binaries
+// ("The Goby Binaries").
+//
+// The Goby Binaries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// The Goby Binaries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Goby.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "goby/util/geodesy.h"
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+
+
+#include <boost/units/io.hpp>
+
+using namespace goby::util;
+
+bool double_cmp(double a, double b, int precision)
+{
+    return std::abs(a-b) < pow(10.0, -precision);
+}
+
+int main()
+{
+    using boost::units::degree::degrees;
+    using boost::units::si::meters;
+    
+    {
+        
+        goby::util::UTMGeodesy geodesy({ 42.177127968804754*degrees, -70.16303866815588*degrees });
+        std::cout << "zone: " << geodesy.origin_utm_zone() << std::endl;
+        assert(geodesy.origin_utm_zone() == 19);
+        
+        auto origin_utm = geodesy.origin_utm();
+        
+        std::cout << "utm origin: " << std::setprecision(std::numeric_limits<double>::digits10) << origin_utm.x << ", " << origin_utm.y << std::endl;
+        
+        assert(double_cmp(origin_utm.x/meters, 403946.82376733015, 3));
+        assert(double_cmp(origin_utm.y/meters, 4670097.454234971, 3));    
+    }
+
+    {
+        goby::util::UTMGeodesy geodesy({ 41*degrees, -70*degrees });
+
+        auto geo = geodesy.convert(goby::util::UTMGeodesy::XYPoint({100*meters, 100*meters}));
+        auto origin_geo = geodesy.origin_geo();
+
+        std::cout << "geo origin: " << std::setprecision(std::numeric_limits<double>::digits10) << origin_geo.lat << ", " << origin_geo.lon << std::endl;
+        std::cout << "(x = 100, y = 100) as (lat, lon): (" << std::setprecision(std::numeric_limits<double>::digits10) << geo.lat << ", " << geo.lon << ")"<< std::endl;
+
+        assert(double_cmp(geo.lat/degrees, 41.00091, 5));
+        assert(double_cmp(geo.lon/degrees, -69.99882, 5));
+
+        auto utm = geodesy.convert(geo);
+        std::cout << "reconvert as (x, y): (" << std::setprecision(std::numeric_limits<double>::digits10) << utm.x << ", " << utm.y << ")"<< std::endl;
+        assert(double_cmp(utm.x/meters, 100, 3));
+        assert(double_cmp(utm.y/meters, 100, 3));
+        
+    }
+    
+    std::cout << "all tests passed" << std::endl;
+    return 0;
+}
---
+++ src/util/geodesy.h
@@ -0,0 +1,76 @@
+// Copyright 2009-2018 Toby Schneider (http://gobysoft.org/index.wt/people/toby)
+//                     GobySoft, LLC (2013-)
+//                     Massachusetts Institute of Technology (2007-2014)
+//                     Community contributors (see AUTHORS file)
+//
+//
+// This file is part of the Goby Underwater Autonomy Project Libraries
+// ("The Goby Libraries").
+//
+// The Goby Libraries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// The Goby Libraries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Goby.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef GobyGeodesy20180312H
+#define GobyGeodesy20180312H
+
+#include <limits>
+
+#include <boost/units/quantity.hpp>
+#include <boost/units/systems/si/length.hpp>
+#include <boost/units/systems/angle/degrees.hpp>
+
+#include <proj_api.h>
+
+#include "goby/util/sci.h"
+
+
+namespace goby
+{
+    namespace util
+    {
+        class UTMGeodesy
+        {
+        public:
+            struct LatLonPoint
+            {
+                boost::units::quantity<boost::units::degree::plane_angle> lat;
+                boost::units::quantity<boost::units::degree::plane_angle> lon;
+            };
+
+            struct XYPoint
+            {
+                boost::units::quantity<boost::units::si::length> x;
+                boost::units::quantity<boost::units::si::length> y;
+            };
+            
+            UTMGeodesy(LatLonPoint origin);
+            virtual ~UTMGeodesy();
+            
+            LatLonPoint origin_geo() { return origin_geo_; }
+            XYPoint origin_utm() { return origin_utm_; }
+            int origin_utm_zone() { return origin_zone_; }
+            
+
+            LatLonPoint convert(XYPoint utm);
+            XYPoint convert(LatLonPoint geo);
+            
+        private:
+            LatLonPoint origin_geo_;
+            int origin_zone_;
+            XYPoint origin_utm_;
+            projPJ pj_utm_, pj_latlong_;
+        };
+    }
+}
+
+#endif
