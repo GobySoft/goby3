e268de1ee67adbd446beb3a5c2c38bd75e690455
Toby Schneider <toby@gobysoft.org>
2019-06-14 05:24:15 -0400

    Added new detail file for primitive type structs

---
+++ src/middleware/detail/primitive_type.h
@@ -0,0 +1,52 @@
+// Copyright 2009-2018 Toby Schneider (http://gobysoft.org/index.wt/people/toby)
+//                     GobySoft, LLC (2013-)
+//                     Massachusetts Institute of Technology (2007-2014)
+//                     Community contributors (see AUTHORS file)
+//
+//
+// This file is part of the Goby Underwater Autonomy Project Libraries
+// ("The Goby Libraries").
+//
+// The Goby Libraries are free software: you can redistribute them and/or modify
+// them under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// The Goby Libraries are distributed in the hope that they will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Goby.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef PRIMITIVE_TYPE_20190614H
+#define PRIMITIVE_TYPE_20190614H
+
+namespace goby
+{
+namespace middleware
+{
+namespace detail
+{
+
+template <typename T> struct primitive_type
+{
+    typedef T type;
+};
+
+template <typename T> struct primitive_type<std::shared_ptr<T> >
+{
+    typedef T type;
+};
+
+template <typename T> struct primitive_type<std::shared_ptr<const T> >
+{
+    typedef T type;
+};
+
+}
+}
+}
+
+#endif
