775b090256f257b4a2fc66d246c91b8e772ebf89
Toby Schneider <toby@gobysoft.org>
2020-12-07 12:50:14 -0500

    Added missing new header

---
+++ src/middleware/application/detail/interprocess_common.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "goby/zeromq/protobuf/interprocess_config.pb.h"
+
+namespace goby
+{
+namespace middleware
+{
+namespace detail
+{
+inline goby::zeromq::protobuf::InterProcessPortalConfig
+make_interprocess_config(goby::zeromq::protobuf::InterProcessPortalConfig cfg, std::string app_name)
+{
+    cfg.set_client_name(app_name);
+    return cfg;
+}
+} // namespace detail
+} // namespace middleware
+} // namespace goby
