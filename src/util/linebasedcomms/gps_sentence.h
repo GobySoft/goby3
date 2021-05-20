1fe8231ab2d46c3e1fefbc05407e051b271aacb0
Toby Schneider <toby@gobysoft.org>
2021-05-20 14:56:54 -0400

    Began adding GPS NMEA-0183 messages to linebasedcomms, starting with RMC

---
+++ src/util/linebasedcomms/gps_sentence.h
@@ -0,0 +1,95 @@
+
+#ifndef GOBY_UTIL_LINEBASEDCOMMS_GPS_SENTENCE_H
+#define GOBY_UTIL_LINEBASEDCOMMS_GPS_SENTENCE_H
+
+#include <boost/format.hpp>
+#include <boost/optional.hpp>
+#include <boost/units/base_units/metric/knot.hpp>
+#include <boost/units/systems/angle/degrees.hpp>
+#include <boost/units/systems/si.hpp>
+#include <boost/units/systems/temperature/celsius.hpp>
+
+#include "goby/time/convert.h"
+#include "goby/time/system_clock.h"
+#include "nmea_sentence.h"
+
+namespace goby
+{
+namespace util
+{
+namespace gps
+{
+/// \brief Convert NMEA latitude/longitude string plus hemisphere to a decimal degrees value
+double nmea_geo_to_decimal(std::string nmea_geo_str, char hemi);
+
+enum CoordType
+{
+    LAT,
+    LON
+};
+
+/// \brief Convert a decimal degrees latitude or longitude into a pair of NMEA string representation and hemisphere value
+std::pair<std::string, char> decimal_to_nmea_geo(double decimal, CoordType t);
+
+struct RMC
+{
+  public:
+    RMC() = default;
+
+    RMC(const NMEASentence& sentence) { parse(sentence); }
+
+    void parse(const NMEASentence& sentence);
+    NMEASentence serialize(std::string talker_id = "GP", int num_fields = min_size) const;
+
+    boost::optional<goby::time::SystemClock::time_point> time;
+
+    boost::optional<boost::units::quantity<boost::units::degree::plane_angle>> latitude;
+    boost::optional<boost::units::quantity<boost::units::degree::plane_angle>> longitude;
+    boost::optional<boost::units::quantity<boost::units::si::velocity>> speed_over_ground;
+    boost::optional<boost::units::quantity<boost::units::degree::plane_angle>> course_over_ground;
+
+    boost::optional<boost::units::quantity<boost::units::degree::plane_angle>> magnetic_variation;
+
+    enum Fields
+    {
+        UTC_TIME = 1,
+        VALIDITY = 2,
+        LATITUDE = 3,
+        LATITUDE_NS = 4,
+        LONGITUDE = 5,
+        LONGITUDE_EW = 6,
+        SPEED_OVER_GROUND = 7,
+        COURSE_OVER_GROUND = 8,
+        DATE = 9,
+        MAGNETIC_VARIATION = 10,
+        MAG_VARIATION_EW = 11,
+        MODE = 12,      // NMEA 2.3 and later
+        NAV_STATUS = 13 // NMEA 4.1 and later
+    };
+
+    enum Status
+    {
+        DataValid,
+        NavigationReceiverWarning
+    };
+
+    boost::optional<Status> status;
+
+    constexpr static int min_size = MAG_VARIATION_EW + 1; // MAG_VARIATION_EW + talker
+};
+
+bool operator==(const RMC& rmc1, const RMC& rmc2)
+{
+    return rmc1.serialize().message() == rmc2.serialize().message();
+}
+
+std::ostream& operator<<(std::ostream& os, const RMC& rmc)
+{
+    return (os << rmc.serialize().message());
+}
+
+} // namespace gps
+} // namespace util
+} // namespace goby
+
+#endif
