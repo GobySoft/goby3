4ff50a48a20b99da471f020122ca28a50192d833
Toby Schneider <toby@gobysoft.org>
2026-05-29 21:05:06 -0400

    Update bookworm and jammy Dockerfiles to use new format used by resolute/trixie

---
+++ .circleci/docker/bookworm/build_and_push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+for arch in amd64 arm64 armhf; do
+    archname=$arch
+    if [[ "$arch" = "amd64" ]]; then
+        archname="base"
+    fi
+    tag="gobysoft/goby3-debian-build-$archname:12.1"
+    docker build --no-cache --build-arg TARGET_ARCH=$arch -t $tag .
+    docker push $tag
+done
